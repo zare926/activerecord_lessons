47dfc7023294fa1b773d4b216a5eb575fb3b48cf
zare926 <ilove.boc926@gmail.com>
2020-06-28 23:30:26 +0900

    find_or_create_by

--- main.rb
+++ main.rb
@@ -15,11 +15,11 @@ ActiveRecord::Base.establish_connection(
 
 class User < ActiveRecord::Base
   # class method
-  def self.top(num)
-    select("id, name, age").order(:age).limit(num)
-  end
-  #scope
-  scope :top, ->(num) {select("id, name, age").order(:age).limit(num)}
+  # def self.top(num)
+  #   select("id, name, age").order(:age).limit(num)
+  # end
+  # #scope
+  # scope :top, ->(num) {select("id, name, age").order(:age).limit(num)}
 end
 
 User.delete_all
@@ -30,5 +30,19 @@ User.create(name:"murata", age:24)
 User.create(name:"suzuki", age:77)
 User.create(name:"okazaki", age:10)
 
-# pp User.top3
-pp User.top(2)
+# find_or_create_by
+
+# 登録されているユーザーの場合は参照できる
+# user = User.find_or_create_by(name:"takahashi")
+# pp user
+
+# 登録されていないユーザーの場合は登録されるが、検索した値以外はnilになる
+# user = User.find_or_create_by(name:"yokota")
+# pp user
+
+# 検索した値以外を入力する場合はブロックを使う
+user = User.find_or_create_by(name:"yokota") do |u|
+  u.age = 18
+end
+pp user
+
