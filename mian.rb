e99b364ce18c0665683a82c1ab7441e62250b0ce
zare926 <ilove.boc926@gmail.com>
2020-06-28 18:05:43 +0900

    ActiveRecordの設定

---
+++ mian.rb
@@ -0,0 +1,16 @@
+require 'active_record'
+# ppはプリティープリント、オブジェクトをわかりやすく表現するrubyのライブラリ
+require 'pp'
+
+# 時間の設定、決まり文句ぐらいで覚える
+Time.zone_default = Time.find_zone! 'Tokyo'
+ActiveRecord::Base.default_timezone = :local
+
+# DBの読み込み
+ActiveRecord::Base.establish_connection(
+  "adaoter" => "sqlite3",
+  "database" => "./myapp.db"
+)
+
+class User < ActiveRecord::Base
+end
