30d69483a5c8cf3b314f554de7a4e3e9100cfadb
zare926 <ilove.boc926@gmail.com>
2020-06-28 18:47:18 +0900

    抽出条件の指定

--- main.rb
+++ main.rb
@@ -24,13 +24,20 @@ User.create(name:"murata", age:24)
 User.create(name:"suzuki", age:77)
 User.create(name:"okazaki", age:10)
 
-# レコードをすべて抽出
-pp User.all
-# 表示したいレコードを選択することが可能
-pp User.select("id, name, age").all
-# # 最初のレコードを抽出する方法
-pp User.select("id, name, age").first
-# 最後のレコードを抽出する方法
-pp User.select("id, name, age").last
-# 最初の3件を抽出する方法
-pp User.select("id, name, age").first(3)
+# IDを指定してレコード抽出する方法
+pp User.select("id, name, age").find(3)
+
+# 別のフィールドで抽出する方法
+pp User.select("id, name, age").find_by(name:"tanaka")
+
+# # 引数の（）をrubyは外せる
+pp User.select("id, name, age").find_by name:"tanaka"
+
+# # find_byの後にカラム名を繋げられる　例:find_by_name "tanaka"
+pp User.select("id, name, age").find_by_name "tanaka"
+
+# # 値は（）をつけてもよし
+pp User.select("id, name, age").find_by_name("tanaka")
+
+# 存在しない値を探した時、エラーを返す場合は命令に!をつける
+pp User.select("id, name, age").find_by_name!("kirya")
