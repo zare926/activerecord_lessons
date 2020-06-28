1c86beb5189b15b34cb68f3741cdf503b187b947
zare926 <ilove.boc926@gmail.com>
2020-06-28 18:33:02 +0900

    Loggerを使った確認方法

--- main.rb
+++ main.rb
@@ -2,10 +2,15 @@ require 'active_support/all'
 require "active_record"
 # ppはプリティープリント、オブジェクトをわかりやすく表現するrubyのライブラリ
 require 'pp'
+# どういったSQLが発行されているか確認するライブラリ？
+require 'logger'
 
 # 時間の設定、決まり文句ぐらいで覚える
 Time.zone_default = Time.find_zone! 'Tokyo'
 ActiveRecord::Base.default_timezone = :local
+
+# 引数に出力先を書く
+ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 # DBの読み込み
 ActiveRecord::Base.establish_connection(
@@ -16,17 +21,11 @@ ActiveRecord::Base.establish_connection(
 class User < ActiveRecord::Base
 end
 
-# # insert
-# DBへの登録するための記述
-user = User.new
-user.name = "tanaka"
-user.age = 23
+# insert
+# ブロックを使ったインサート方法
+user = User.new do |u|
+  u.name = "sato"
+  u.age = 19
+end
 user.save
-
-# ロケット
-user = User.new(:name => "hayashi", :age => 25)
-user.save
-
-# createを使うとnewとsaveは同時にできる
-user = User.create(name: "nakamura", age: 26)
 
