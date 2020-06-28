99bfb76cfec907579dc273834a8062c846ed8c35
zare926 <ilove.boc926@gmail.com>
2020-06-28 18:28:34 +0900

    レコードの挿入方法

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
-require 'active_record'
+require 'active_support/all'
+require "active_record"
 # ppはプリティープリント、オブジェクトをわかりやすく表現するrubyのライブラリ
 require 'pp'
 
@@ -8,9 +9,24 @@ ActiveRecord::Base.default_timezone = :local
 
 # DBの読み込み
 ActiveRecord::Base.establish_connection(
-  "adaoter" => "sqlite3",
+  "adapter" => "sqlite3",
   "database" => "./myapp.db"
 )
 
 class User < ActiveRecord::Base
 end
+
+# # insert
+# DBへの登録するための記述
+user = User.new
+user.name = "tanaka"
+user.age = 23
+user.save
+
+# ロケット
+user = User.new(:name => "hayashi", :age => 25)
+user.save
+
+# createを使うとnewとsaveは同時にできる
+user = User.create(name: "nakamura", age: 26)
+
